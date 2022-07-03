8a11fcad43ff4e309bcb8f5ac4fab1e03930ad80
psj98 <park98sj1@hanmail.net>
2022-07-04 01:02:42 +0900

    c++ 공부

--- Test Folder/test_overloading2.cpp
+++ Test Folder/test_overloading2.cpp
@@ -4,65 +4,76 @@ using namespace std;
 
 class Time {
 private:
-	int hours;
-	int mins;
+  int hours;
+  int mins;
 
 public:
-	Time();
-	Time(int, int);
-	~Time();
-	Time operator+(Time&);
-	void addHours(int);
-	void addMins(int);
-	void show();
+  Time();
+  Time(int, int);
+  ~Time();
+  void addHours(int);
+  void addMins(int);
+  int getHours();
+  int getMins();
+  void show();
+  Time operator+(Time&);
 };
 
 int main() {
-	Time day1(1, 40);
-	Time day2(2, 30);
-	
-	day1.show();
-	day2.show();
+  Time t1(3, 20);
+  Time t2(1, 45);
 
-	Time total;
-	total = day1.operator+(day2);
+  t1.show();
+  t2.show();
+
+  Time total;
+  
+	total = t1.operator+(t2);
 	total.show();
-
-	return 0;
+  
+  return 0;
 }
 
-Time::Time() {
-	hours = mins = 0;
+Time::Time(){
+  hours = mins = 0;
 }
 
-Time::Time(int h, int m) {
-	hours = h;
-	mins = m;
+Time::Time(int h, int m){
+  hours = h;
+  mins = m;
 }
 
-void Time::addHours(int h) {
-	hours += h;
-};
+Time::~Time(){}
 
-void Time::addMins(int m) {
-	mins += m;
-	hours += mins / 60;
-	mins %= 60;
-};
+void Time::addHours(int h){
+  hours += h;
+}
 
-Time Time::operator+(Time& t) {
-	Time sum;
+void Time::addMins(int m){
+  mins += m;
+  hours += mins / 60;
+  mins %= 60;
+}
 
-	sum.mins = mins + t.mins;
+int Time::getHours(){
+  return hours;
+}
+
+int Time::getMins(){
+  return mins;
+}
+
+void Time::show(){
+  printf("hh:mm - %d:%d\n", hours, mins);
+}
+
+Time Time::operator+(Time& t){
+  Time sum;
+
+  sum.mins = mins + t.mins;
 	sum.hours = hours + t.hours;
 	sum.hours += sum.mins / 60;
 	sum.mins %= 60;
 
 	return sum;
-};
-
-void Time::show() {
-	printf("½Ã°£:ºÐ - %d:%d\n", hours, mins);
 }
-
-Time::~Time() {}
