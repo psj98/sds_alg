39d568a20b35dbfafa0597709fee04f686da3533
psj98 <park98sj1@hanmail.net>
2022-07-03 21:09:47 +0900

    c++ test

--- Test Folder/test.cpp
+++ Test Folder/test.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main(){
-    cout << "Hello" << endl;
+    char line[30];
+    string a;
+    int t[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    cin.getline(line, 30);
+    getline(cin, a);
+
+    cout << line << '\n';
+    cout << a << '\n';
+    
+    int row = sizeof(t)/sizeof(t[0]);
+    int col = sizeof(t[0])/sizeof(int);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            cout << t[i][j] << endl;
+
     return 0;
 }
